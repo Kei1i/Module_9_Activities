b98d90a1cc98c2009ff32351f1fe77f5cda07970
Keisha <2225253@slu.edu.ph>
2022-04-05 21:50:45 +0800

    1

--- Graded_Activity_7/Kimmakim_Dim2.java
+++ Graded_Activity_7/Kimmakim_Dim2.java
@@ -5,7 +5,7 @@
  * Monday Wednesday (10:30 am - 1:30 pm)
  * @version "17.0.2" 2022-01-18
  */
-package Graded_Activity_8;
+package Graded_Activity_7;
 import java.util.*;   
 import java.lang.Math;
 public class Kimmakim_Dim2 {
